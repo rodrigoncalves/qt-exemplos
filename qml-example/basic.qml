5da59bc251123329daba5fd19b0a135dab0daa1a
Rodrigo Gonçalves <rgoncalves@esec.com.br>
2016-12-13 11:39:29 -0200

    Deploying to C++ application

---
+++ qml-example/basic.qml
@@ -0,0 +1,15 @@
+import QtQuick 2.5
+import QtQuick.Controls 1.4
+
+ApplicationWindow {
+    width: 300
+    height: 200
+    title: "Simple"
+
+    Text {
+        text: "Qt Quick"
+        anchors.horizontalCenter: parent.horizontalCenter
+        anchors.verticalCenter: parent.verticalCenter
+        font.pointSize: 24
+    }
+}
---
+++ qml-example/main.cpp
@@ -0,0 +1,17 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQuickWindow>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/simple.qml")));
+
+    QObject *topLevel = engine.rootObjects().value(0);
+    QQuickWindow *window = qobject_cast<QQuickWindow *>(topLevel);
+    window->show();
+
+    return app.exec();
+}
